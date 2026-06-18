dee5c28c16615f065950b7cbfa22d9970a77d326
QuiqueDogo <enrique.lgv93@gmail.com>
2026-06-18 00:58:09 -0600

    fix: areglamos cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,11 +7,23 @@ from app.models.user import User
 
 from app.routes.users import router as users_router
 from app.exceptions import generic_exception_handler
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Users API",
     description="PruebaFullstack Developer",
     version="1.0.0",
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:8000",
+        
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 Base.metadata.create_all(bind=engine)
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,7 @@ import "antd/dist/reset.css";
 ReactDOM.createRoot(
   document.getElementById("root")
 ).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
 );
